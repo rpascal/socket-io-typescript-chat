aa9218a6098523497cadac7722ce168a3156147f
Unknown <ryanpascal99@yahoo.com>
2018-04-11 23:00:31 -0400

    Minor changes

--- bin/installDependencies.sh
+++ bin/installDependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-current_dir=$PWD;cd client;npm install;cd $current_dir;
-current_dir=$PWD;cd server;npm install;cd $current_dir;
+current_dir=$PWD;cd client;sudo npm install;cd $current_dir;
+current_dir=$PWD;cd server;sudo npm install;cd $current_dir;
